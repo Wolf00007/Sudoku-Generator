6c43e3ffb4ece4fde625f2bae43676c2064c3fd0
Wolf <wgrala@gmail.com>
2021-01-04 17:37:42 +0100

    -fixed the generator to correctly create a valid SuDoKu puzzle -implemented the backtracking algorithm (solveBT)

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
 import random
-
-# the sudoku grid
-
-# +-------+-------+-------+
-# | 0 0 0 | 0 0 0 | 0 0 0 |
-# | 0 0 0 | 0 0 0 | 0 0 0 |
-# | 0 0 0 | 0 0 0 | 0 0 0 |    #The classic Sudoku game involves a grid of 81 squares.
-# +-------+-------+-------+    # The grid is divided into nine blocks, each containing nine squares.
-# | 0 0 0 | 0 0 0 | 0 0 0 |    # The rules of the game are simple:
-# | 0 0 0 | 0 0 0 | 0 0 0 |       # each of the nine blocks has to contain all the numbers 1-9 within its squares.
-# | 0 0 0 | 0 0 0 | 0 0 0 |       # Each number can only appear once in a row, column or box.
-# +-------+-------+-------+       # The difficulty lies in that each vertical nine-square column,
-# | 0 0 0 | 0 0 0 | 0 0 0 |       # or horizontal nine-square line across, within the larger square,
-# | 0 0 0 | 0 0 0 | 0 0 0 |       # must also contain the numbers 1-9, without repetition or omission.
-# | 0 0 0 | 0 0 0 | 0 0 0 |    # Every puzzle has just one correct solution.
-# +-------+-------+-------+
-
-# option to choose difficulty level
-
-print("Level of Difficulty")
-print("     1. Beginner")
-print("     2. Intermediate")
-print("     3. Advanced")
-print()
-print("Enter the level of difficulty as per your choice:  ",end="")
-level = int(input())
-if level==1:
-    q = 35
-elif level==2:
-    q = 20
-else:
-    q = 8
-#place to store the grid
+from random import randint, shuffle
 
 grid = [[0 for x in range(9)] for y in range(9)]
+numberList=[1,2,3,4,5,6,7,8,9]
+#shuffle(numberList)
 
 # validating the grid
-
 def check_valid(mesh,r,c,n):
     valid = True
     #check row and column
@@ -58,31 +27,202 @@ def check_valid(mesh,r,c,n):
                 break
     return valid
 
-# generating random values for the grid
-for i in range(q):
-    row = random.randrange(9)
-    col = random.randrange(9)
-    num = random.randrange(1,10)
-    while not check_valid(grid,row,col,num) or grid[row][col]!=0:
-        row = random.randrange(9)
-        col = random.randrange(9)
-        num = random.randrange(1,10)
-    grid[row][col]=num
-
-
 # print the grid
+def print_board(grid):
 
-cnt_i = 0
-for i in range(13):
-    cnt_j = 0
-    for j in range(13):
-        if i%4==0:
-            print("+---------+---------+---------+",end="")
-            cnt_i+=1
-            break
-        elif j%4==0:
-            print("|",end="")
-            cnt_j+=1
-        else:
-            print("",grid[i-cnt_i][j-cnt_j],"" ,end="")
-    print()
+    cnt_i = 0
+    for i in range(13):
+        cnt_j = 0
+        for j in range(13):
+            if i%4==0:
+                print("+---------+---------+---------+",end="")
+                cnt_i+=1
+                break
+            elif j%4==0:
+                print("|",end="")
+                cnt_j+=1
+            else:
+                print("",grid[i-cnt_i][j-cnt_j],"" ,end="")
+        print()
+
+# find an empty square (0)
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range (len(grid[0])):
+            if grid[i][j] == 0:
+                return (i, j) # row, col
+    return None
+
+#A function to check if the grid is full
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+
+  #We have a complete grid!
+  return True
+
+# solve sudoku
+def solveBT(grid):
+
+    #print(grid)
+    find = find_empty(grid)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range (1,10):
+        if check_valid(grid, row, col, i):
+            grid[row][col] = i
+
+            if solveBT(grid):
+                return True
+
+            grid[row][col] = 0
+
+    return False
+
+#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+def initialSolve(grid):
+  global counter
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      for value in range (1,10):
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                counter+=1
+                break
+              else:
+                if initialSolve(grid):
+                  return True
+      break
+  grid[row][col]=0
+
+
+
+#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+def fillGrid(grid):
+  global counter
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      shuffle(numberList)
+      for value in numberList:
+        #Check that this value has not already be used on this row
+        if not(value in grid[row]):
+          #Check that this value has not already be used on this column
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            #Identify which of the 9 squares we are working on
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                return True
+              else:
+                if fillGrid(grid):
+                  return True
+      break
+  grid[row][col]=0
+
+
+#Generate a Fully Solved Grid
+fillGrid(grid)
+print_board(grid)
+
+# Start Removing Numbers one by one
+
+# A higher number of attempts will end up removing more numbers from the grid
+# Potentially resulting in more difficult grids to solve!
+attempts = 5
+print ("Attempts: ", attempts)
+counter = 1
+while attempts > 0:
+    # Select a random cell that is not already empty
+    row = randint(0, 8)
+    col = randint(0, 8)
+    while grid[row][col] == 0:
+        row = randint(0, 8)
+        col = randint(0, 8)
+    # Remember its cell value in case we need to put it back
+    backup = grid[row][col]
+    grid[row][col] = 0
+
+    # Take a full copy of the grid
+    copyGrid = []
+    for r in range(0, 9):
+        copyGrid.append([])
+        for c in range(0, 9):
+            copyGrid[r].append(grid[r][c])
+
+    # Count the number of solutions that this grid has (using a backtracking approach implemented in the initialSolve() function)
+    counter = 0
+    initialSolve(copyGrid)
+    # If the number of solution is different from 1 then we need to cancel the change by putting the value we took away back in the grid
+    if counter != 1:
+        grid[row][col] = backup
+        # We could stop here, but we can also have another attempt with a different cell just to try to remove more numbers
+        attempts -= 1
+
+print_board(grid)
+print("Sudoku Grid Ready")
+solveBT(grid)
+print_board(grid)
+print("Backtracking algorithm solution")
